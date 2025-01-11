935e58e80910db6701514e0d861e24abf1be43a4
Isimbi <isimbiange19@gmail.com>
2025-01-10 18:35:53 -0600

    Time: 30 ms (0.73%), Space: 16.7 MB (89.69%) - LeetHub

---
+++ 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution(object):
+    def equalFrequency(self, word):
+        counter = Counter(word)
+        for c in word:
+            counter[c] -= 1
+            
+            if counter[c] == 0:
+                del counter[c]
+            
+            if len(set(counter.values())) == 1:
+                return True
+
+            counter[c] += 1
+        return False
