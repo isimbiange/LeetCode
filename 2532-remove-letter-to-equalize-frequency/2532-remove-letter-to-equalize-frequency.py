c5f1db9251301b736b5a6e16785cd3f843b5473e
Isimbi <isimbiange19@gmail.com>
2025-01-17 23:44:09 -0600

    Time: 0 ms (100%), Space: 12.5 MB (25.83%) - LeetHub

--- 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
@@ -2,18 +2,16 @@ from collections import Counter
 
 class Solution(object):
     def equalFrequency(self, word):
-        counter = Counter(word)
-        for c in word:
-            counter[c] -= 1
-            
-            if counter[c] == 0:
-                del counter[c]
-            
-            if len(set(counter.values())) == 1:
+        freq = Counter(word)
+        for char in word:
+            freq[char] -= 1
+            if freq[char]==0:
+                freq.pop(char)
+
+            if len(set(freq.values()))==1:
                 return True
+            freq[char] +=1
 
-            counter[c] += 1
         return False
-
-        
-        
+            
+            
