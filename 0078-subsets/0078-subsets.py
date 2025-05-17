6de1f6887fa9c4aa7e93839da6f821930a01dcc5
Isimbi <isimbiange19@gmail.com>
2025-05-17 16:05:38 -0500

    Time: 0 ms (100%), Space: 18 MB (21.17%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,10 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
 
-        result = [[]]
+        result = [[]] #empty subset
 
-        for num in nums:
-            result += [subset +[num] for  subset in result]
+        for num in nums: #looping through the list(subset)
+            result += [subset +[num] for  subset in result] #extends the current result by adding a new subset
+            #for each existing subset in results create a new subset by adding num
 
         return result
         
