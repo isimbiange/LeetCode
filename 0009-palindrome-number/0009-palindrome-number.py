ee6b1bfcc68469653a77b496351c9db55a23ccc3
Isimbi <isimbiange19@gmail.com>
2025-05-27 23:50:40 -0500

    Time: 11 ms (35.85%), Space: 17.9 MB (39.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,22 +1,23 @@
 class Solution(object):
     def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
         s = str(x)
-        if s =='':
+        
+        # Handle negative numbers
+        if s[0] == '-':
             return False
-
-        left,right=0 , len(s)-1
+        
+        # Initialize pointers
+        left, right = 0, len(s) - 1
+        
+        # Check if the string is a palindrome
         while left < right:
-
-            if s[left]!= s[right]:
+            if s[left] != s[right]:
                 return False
-
             left += 1
             right -= 1
-        return True
-
-
-    
-    
-   
-           
         
+        return True
