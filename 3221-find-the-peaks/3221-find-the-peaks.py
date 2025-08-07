b8691ff217c5731a4c441afb9ef383b73e5bd2db
Isimbi <isimbiange19@gmail.com>
2025-08-07 05:52:13 -0500

    Time: 0 ms (100%), Space: 17.8 MB (34.55%) - LeetHub

--- 3221-find-the-peaks/3221-find-the-peaks.py
+++ 3221-find-the-peaks/3221-find-the-peaks.py
@@ -1,12 +1,12 @@
 class Solution:
     def findPeaks(self, mountain: List[int]) -> List[int]:
-        peaks = []
+        peaks = [] # a new list that will store the peaks
 
-        for i in range(1,len(mountain) -1):
-            if mountain[i] > mountain[i-1] and mountain[i] > mountain[i+1]:
-                peaks.append(i)
+        for i in range(1,len(mountain) -1): # iterate through the mountains from the second first and last since they are in the middle
+            if mountain[i] > mountain[i-1] and mountain[i] > mountain[i+1]: #if one of the mountains is greater than the both surrounding mountains
+                peaks.append(i) #add its index to the peaks list
 
-        return peaks
+        return peaks #return those index of the mountains that were peaks
         
 
         
