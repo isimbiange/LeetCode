e870a396b57afbc69554612c33a961fe50bed5a2
Isimbi <isimbiange19@gmail.com>
2025-09-17 01:47:09 -0500

    Time: 11 ms (34.93%), Space: 23.1 MB (17.37%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        filtered = []
+        filtered = []# new list for palindrome string
 
-        for char in s:
-            if char.isalnum():
-                filtered.append(char.lower())
-        cleaned = "".join(filtered)
+        for char in s:#iterate through the string
+            if char.isalnum():# check if char is numeric or alphabet
+                filtered.append(char.lower()) # put the char together by making all characters lower characters
+        cleaned = "".join(filtered)#remove space 
 
-        return cleaned == cleaned[::-1]
+        return cleaned == cleaned[::-1] #retirn True if the reversed string is equal to the string we were given
         
         
