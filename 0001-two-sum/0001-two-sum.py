acdc60bf7d6c9166d5bebfec3b762426518716a3
Isimbi <isimbiange19@gmail.com>
2025-09-24 00:50:44 -0500

    Time: 0 ms (100%), Space: 18.8 MB (62.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,8 +5,8 @@ class Solution:
         for i, num in enumerate(nums): #loop through the list of numbers
             complement = target - num #linear subtract target from the all the numbers if to find compement
 
-            if complement in num_dict: # is the complement exist in the list
-                return [num_dict[complement], i] #return the imdex of the number in the list
+            if complement in num_dict: # if the complement exist in the nums list
+                return [num_dict[complement], i] #return the index of the number in the list
 
             num_dict[num] = i
         return none #if the complement not in the list return none
