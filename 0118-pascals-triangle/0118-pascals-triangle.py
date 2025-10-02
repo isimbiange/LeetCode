a897757d235a4310f8815864468790e3e5a38922
Isimbi <isimbiange19@gmail.com>
2025-10-02 07:26:07 -0500

    Time: 0 ms (100%), Space: 17.8 MB (37.7%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,12 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res=[[1]] #the first row we already have
+        triangle = [[1]]
+        for i in range (numRows - 1): #since we already have the one row
 
-        for i in range(numRows-1): # iterating starting from our one row
-            temp = [0]+res[-1]+[0] #adding 0 on both sides of the row
-            row =[] #our new empty row
-            for j in range (len(res[-1])+1): #adding the next pointer to the second pointer
-                row.append(temp[j]+temp[j+1]) # findind the additional result
-            res.append(row)#appending the new result in our empty row
-        return res #retun the number of rows we had to go through to much our input
+            prev = triangle[-1] #starting from the first row
+
+            new_row = [a+b for a , b in zip ([0] + prev, prev + [0])]# add 0-s on both sides after the first row
+             
+            triangle.append(new_row)# a name can't have a dot is for attributes
+
+        return triangle
+        
         
