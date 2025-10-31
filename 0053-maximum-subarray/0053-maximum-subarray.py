8d0b610db3e4815aef4c721157dbd969673d1ea7
Isimbi <isimbiange19@gmail.com>
2025-10-31 18:47:25 -0500

    Time: 85 ms (43.69%), Space: 32.1 MB (66.33%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,13 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0] #start with the first number in nums as its sum
-        #Kadane's algorithm/dynamic programming)
-        curr_sum = 0 #equal to 0 until we add other numbers
+        max_sum = nums[0]
+        curr_sum = 0
 
         for num in nums:
             curr_sum = max(curr_sum + num, num)
-            #if curr sum +num is < and num is > you go with the buggest. , means or
-            max_sum = max (max_sum, curr_sum)
+            max_sum= max(curr_sum, max_sum)
 
         return max_sum
-        
