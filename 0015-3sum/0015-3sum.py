f589fdec158fca4da400a2b7c2fa906178044c5e
Isimbi <isimbiange19@gmail.com>
2025-11-17 20:39:50 -0600

    Time: 539 ms (72.35%), Space: 20.7 MB (71.08%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
-        
+        res = []
 
-        for i, a in enumerate (nums): #use each number in the array as a possible first input value
-            if i > 0 and a == nums[i-1]: # make sure we do not reuse any number twice
+        for i, a in enumerate(nums):
+            # Skip duplicate anchors
+            if i > 0 and a == nums[i - 1]:
                 continue
 
-            l,r = i+1, len(nums)-1
+            l, r = i + 1, len(nums) - 1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
+                total = a + nums[l] + nums[r]
+
+                if total < 0:
                     l += 1
+                elif total > 0:
+                    r -= 1
                 else:
                     res.append([a, nums[l], nums[r]])
+
+                    # Move both pointers and skip duplicates
                     l += 1
-                    while nums[l] == nums[l-1] and l < r:
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
 
         return res
-
-                
-
-
-
-
-
-        
